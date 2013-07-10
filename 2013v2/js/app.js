0577e8c07f0f85d5bfe0c5ece93e24e35f4a34ac
achariam <matthew.achariam@gmail.com>
2013-07-10 14:49:13 -0400

    fixes

--- 2013v2/js/app.js
+++ 2013v2/js/app.js
@@ -1,1 +1,8 @@
-$(function() {});
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $('p').css('opacity','0');
+  });
+  $("#show").click(function(){
+    $("p").show();
+  });
+})
